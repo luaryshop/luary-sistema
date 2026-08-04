736bd63e39810971fc2d4cb8d2608497ae8c06b9
luaryshop <luarysemijoias@gmail.com>
2026-08-04 22:33:22 +0000

    Atualiza Luary Shop: gerador de conteúdo universal por marketplace, cadastro de produto universal, preço de equilíbrio

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  // Publicando em usuario.github.io/NOME-DO-REPO: troque para '/NOME-DO-REPO/'
-  base: './'
-});
