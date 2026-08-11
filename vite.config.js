faae760f73015b6e2633520b9b410743a366ea82
luaryshop <luarysemijoias@gmail.com>
2026-08-11 06:42:22 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  // Site publicado em https://SEU-USUARIO.github.io/luary-shop/
-  base: '/luary-shop/'
-});
