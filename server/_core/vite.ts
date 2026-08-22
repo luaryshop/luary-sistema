1389f5cf1408bb9ab4b97164a149fdca57801f86
luaryshop <luarysemijoias@gmail.com>
2026-08-22 00:19:03 +0000

    Corrige carregamento do vite em producao (estava quebrando o boot no Cloud Run)

--- server/_core/vite.ts
+++ server/_core/vite.ts
@@ -1,12 +1,17 @@
 import express, { type Express } from "express";
 import fs from "fs";
 import { type Server } from "http";
-import { nanoid } from "nanoid";
 import path from "path";
-import { createServer as createViteServer } from "vite";
-import viteConfig from "../../vite.config";
 
 export async function setupVite(app: Express, server: Server) {
+  const viteModuleName = "vite";
+  const nanoidModuleName = "nanoid";
+  const viteConfigPath = "../../vite.config";
+
+  const { createServer: createViteServer } = await import(viteModuleName);
+  const { nanoid } = await import(nanoidModuleName);
+  const { default: viteConfig } = await import(viteConfigPath);
+
   const serverOptions = {
     middlewareMode: true,
     hmr: { server },
@@ -32,7 +37,6 @@ export async function setupVite(app: Express, server: Server) {
         "index.html"
       );
 
-      // always reload the index.html file from disk incase it changes
       let template = await fs.promises.readFile(clientTemplate, "utf-8");
       template = template.replace(
         `src="/src/main.tsx"`,
@@ -60,7 +64,6 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
